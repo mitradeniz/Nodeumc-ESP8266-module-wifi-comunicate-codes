e61920135edf706ae977b1368ca726f06691dbf2
mitradeniz <65504062+mitradeniz@users.noreply.github.com>
2023-07-10 22:03:07 +0300

    Update esp8266_gelistirme.ino
    TCP socket kullanılarak 8080 portuna iot cihazımızın ip adresine '1' ve '0' olmak üzere veri yayını yapmaktayız.

--- esp8266_gelistirme.ino
+++ esp8266_gelistirme.ino
@@ -2,6 +2,10 @@
 
 const char* ssid = "Zeynel";
 const char* password = "mitradeniz63";
+const char* serverIP = "192.168.1.15";
+const int serverPort = 8080;
+
+WiFiClient client;
 
 int led = D0;
 
@@ -16,19 +20,36 @@ void setup() {
   
   }
   Serial.println("Wifi baglandii.");
+
+  Serial.print("IP Adresi: ");
+  Serial.println(WiFi.localIP());
+
+
+  
 }
 
 void loop() {
-    if (Serial.available()) {
-      int data = Serial.read();
-      if (data == '1') {
-        digitalWrite(led, HIGH);
-        Serial.println("Hareket algılandı!");
-        
-      } else if (data == '0') {
-        digitalWrite(led, LOW);
-        Serial.println("Hareket yok!");
+    if (Serial.available()) { // hareket verisi var ise 
+      if (client.connect(serverIP, serverPort)) { // wifi ip ve port bağlantısı sağlanıyorsa
+        int data = Serial.read();   // pir sensorunden gelen veriyi oku
+        if (data == '1') {
+          digitalWrite(led, HIGH);
+          client.println('1');      // '1' verisini 8080 portunda yayınla
+          
+        } else if (data == '0') {
+          digitalWrite(led, LOW);
+          client.println('0');
+        }
+      // Sunucu response varsa al
+      while (client.available()) {
+        String response = client.readStringUntil('\n');
+        Serial.println("Sunucu yaniti: " + response);
       }
-      delay(100);
+      
+      // Soketi durdur
+      client.stop();
+    }
+    
+    delay(1000);
   }
 }
