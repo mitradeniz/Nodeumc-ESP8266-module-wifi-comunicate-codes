e2510f047d7ae6abf0eee95a427dccd09b0ad395
mitradeniz <65504062+mitradeniz@users.noreply.github.com>
2023-08-28 14:32:08 +0300

    Update esp8266_gelistirme.ino

--- esp8266_gelistirme.ino
+++ esp8266_gelistirme.ino
@@ -1,3 +1,25 @@
+/*
+  Project Description:
+  This code is part of a project that involves interfacing an ESP8266 module with a PIR motion sensor to detect motion. The ESP8266 connects to a Wi-Fi network and communicates with a server to transmit motion data. This code establishes a connection to the server using Wi-Fi, reads data from the PIR sensor, and sends motion information to the server.
+
+  Libraries Used:
+  - ESP8266WiFi: Used to handle Wi-Fi connectivity and communication.
+  
+  Hardware Setup:
+  - Connect the PIR motion sensor to the digital pin D0 of the ESP8266.
+  
+  Notes:
+  - Replace "--SSID--" and "*********" with your Wi-Fi network SSID and password.
+  - Modify "serverIP" with the IP address of the server where motion data will be sent.
+  - The server should be listening on port 8080 for incoming connections.
+  - When motion is detected, the LED connected to pin D0 is turned on, and the code sends '1' to the server.
+  - When no motion is detected, the LED is turned off, and the code sends '0' to the server.
+  - Responses from the server are displayed on the serial monitor.
+
+  Author: [Your Name]
+  GitHub Repository: [Provide the GitHub repository URL]
+*/
+
 #include <ESP8266WiFi.h>
 
 const char* ssid = "--SSID--";
@@ -16,7 +38,7 @@ void setup() {
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Wifi connection is waiting...");
+    Serial.println("Waiting for Wi-Fi connection...");
   
   }
   Serial.println("Wifi connected.");
